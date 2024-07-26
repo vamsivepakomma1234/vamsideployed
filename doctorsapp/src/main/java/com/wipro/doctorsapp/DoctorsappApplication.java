d1c532bcbf3a097728ed159157c7279fb1a7bda8
vamsivepakomma1234 <54387508+vamsivepakomma1234@users.noreply.github.com>
2024-07-26 14:47:16 +0530

    added

---
+++ doctorsapp/src/main/java/com/wipro/doctorsapp/DoctorsappApplication.java
@@ -0,0 +1,14 @@
+package com.wipro.doctorsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsappApplication.class, args);
+	}
+
+}
+
